5facc0eed5e5ec0d31c7095061b0660aff4ecd08
Ihor-Youx <site7company@gmail.com>
2019-08-21 22:43:58 +0300

    Saving files before refreshing line endings

--- includes/set_bets_template.php
+++ includes/set_bets_template.php
@@ -1,12 +1,21 @@
-<?php 
-// устанавливаем шаблон для записей Ставки
-function load_bets_wp_template( $template ) {
-    global $post;
-    // роверяем не определен ли он в родительской или дочерней теме
-    if ( 'bet' === $post->post_type ) {
-
-        return plugin_dir_path(__FILE__) . 'templates/single-bet.php';
-    }
-
-    return $template;
+<?php 
+
+// устанавливаем шаблон для записей Ставки
+
+function load_bets_wp_template( $template ) {
+
+    global $post;
+
+    // проверка на тип поста
+
+    if ( 'bet' === $post->post_type ) {
+
+        return plugin_dir_path(__FILE__) . 'templates/single-bet.php';
+
+    }
+
+
+
+    return $template;
+
 }
--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1,39 +1,74 @@
-<?php 
-
-// Шоткод формы добавления ставки
-function form_add_bet( $atts ){
-	if( is_user_logged_in() ){
-	$form = '
-	<form id="add_bet_wp">
-	<input type="text" name="title_bet" placeholder="'. __('Заголовок', 'bets-wp') .'" />
-	<textarea name="description_bet" placeholder="'. __('Описание', 'bets-wp') .'"></textarea>
-	<input type="text" name="role" value="'. get_current_user_id() .'" hidden />';
-
-	$terms_type_bets = get_terms(array(
-		'taxonomy' => 'type_bets',
-		"hide_empty" => false
-		)
-		);
-	if( $terms_type_bets && ! is_wp_error($terms_type_bets) ){
-		$form .= '
-		<h3>'. __('Тип ставки', 'bets-wp') .'</h3>
-		<select name="type_list">';
-		foreach ($terms_type_bets as $term_type_bets) {
-			
-			$form .= '<option value="'. $term_type_bets->slug .'">'. $term_type_bets->name .'</option>';
-
-		}
-		$form .= '</select>';
-	}
-
-	$form .= '
-	<input type="submit" value="'. __('Отправить', 'bets-wp') .'" />
-	</form>
-	<div id="response_bets_wp"></div>';
-
-	return $form;
-
-	} else {
-		return __('Вы должны авторизоваться, чтобы создать ставку!', 'bets-wp');
-	}
+<?php 
+
+// Шоткод формы добавления ставки
+
+function form_add_bet( $atts ){
+
+	if( is_user_logged_in() ){
+
+	$form = '
+
+	<form id="add_bet_wp">
+
+	<input type="text" name="title_bet" placeholder="'. __('Заголовок', 'bets-wp') .'" />
+
+	<textarea name="description_bet" placeholder="'. __('Описание', 'bets-wp') .'"></textarea>
+
+	<input type="text" name="role" value="'. get_current_user_id() .'" hidden />';
+
+
+
+	$terms_type_bets = get_terms(array(
+
+		'taxonomy' => 'type_bets',
+
+		"hide_empty" => false
+
+		)
+
+		);
+
+	if( $terms_type_bets && ! is_wp_error($terms_type_bets) ){
+
+		$form .= '
+
+		<h3>'. __('Тип ставки', 'bets-wp') .'</h3>
+
+		<select name="type_list">';
+
+		foreach ($terms_type_bets as $term_type_bets) {
+
+			
+
+			$form .= '<option value="'. $term_type_bets->slug .'">'. $term_type_bets->name .'</option>';
+
+
+
+		}
+
+		$form .= '</select>';
+
+	}
+
+
+
+	$form .= '
+
+	<input type="submit" value="'. __('Отправить', 'bets-wp') .'" />
+
+	</form>
+
+	<div id="response_bets_wp"></div>';
+
+
+	return $form;
+
+
+
+	} else {
+
+		return __('Вы должны авторизоваться, чтобы создать ставку!', 'bets-wp');
+
+	}
+
 }
